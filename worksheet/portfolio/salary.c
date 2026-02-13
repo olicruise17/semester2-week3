e4bb9bab8dd18f3a026b46ae641bbc80d6244d73
Oli Cruise <cruiseoli@outlook.com>
2026-02-13 17:00:54 +0000

    Worksheet complete

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,37 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Oliver Cruise
+ * ID: 201823845
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
 
+   float salary = 36250;
+   float insurance = 8;
+   float tax = 15;
+
     // calculate the deductions and final take-home salary
 
+    // calculate national insurance and calculate salary left after deduction
+
+    float insurance_ded = salary * (insurance/100);
+    float remaining_sal = salary - insurance_ded;
+
+    // calculate taxable income, tax deduction and salary left after deduction
+
+    float taxable_income = remaining_sal - 12500;
+    float tax_ded = taxable_income * (tax/100);
+    float take_home = remaining_sal - tax_ded;
+
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+
+    printf("Salary £%.2f\n",salary);
+    printf("NI contribution £%.2f\n",insurance_ded);
+    printf("Tax contribution £%.2f\n",tax_ded);
+    printf("Take home salary £%.2f\n",take_home);
 
     return 0;
  }
